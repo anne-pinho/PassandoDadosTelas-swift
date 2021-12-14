045a32aa6269c4b55815be131e9ef52d4b33e301
anne-pinho <anne.pinhov@gmail.com>
2021-12-14 10:40:56 -0400

    praticando segues e passar dados inseridos entre telas

---
+++ Pasando Dados/DetalhesViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DetalhesViewController.swift
+//  Pasando Dados
+//
+//  Created by Carol Pinho on 14/12/21.
+//
+
+import UIKit
+
+class DetalhesViewController: UIViewController {
+
+    @IBOutlet weak var resultadoLegenda: UILabel!
+    var textoRecebido: String = "0"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        resultadoLegenda.text = textoRecebido
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Pasando Dados/ViewController.swift
+++ Pasando Dados/ViewController.swift
@@ -8,7 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var nomeCampo: UITextField!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "enviarDados" {
+            let vcDestino = segue.destination as! DetalhesViewController
+            vcDestino.textoRecebido = nomeCampo.text!
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
